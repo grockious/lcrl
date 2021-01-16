61347afa3ecfc801b836ba3776a5a17745d08b13
Hosein Hasanbeig <Grockious@gmail.com>
2021-01-16 17:50:43 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='lcrl',
     version='0.0.2',
+    author="Hosein Hasanbeig",
+    author_email="hosein.hasanbeig@cs.ox.ac.uk",
     keywords='rl, logic, environment, agent',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/grockious/lcrl',
     description='Logically-Constrained Reinforcement Learning',
     packages=['automata', 'environments', 'scripts'],
+    classifiers=[
+        "Provably-Correct RL :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires='>=3.5',
     install_requires=[
         'numpy',
