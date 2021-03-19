a93eb087bb06251115cde3b32ea761d044b7bfdb
Hosein Hasanbeig <Grockious@gmail.com>
2021-03-19 09:37:42 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version='0.0.4',
     author="Hosein Hasanbeig",
     author_email="hosein.hasanbeig@cs.ox.ac.uk",
-    keywords='rk, logic, environment, agent',
+    keywords='rl, logic, environment, agent',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/grockious/lcrl',
